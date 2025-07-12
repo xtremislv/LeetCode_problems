456d1726d141a5146f39b7ba56b80d3b3a4c7726
Lovy Verma <106698656+xtremislv@users.noreply.github.com>
2025-07-12 18:38:17 +0530

    Time: 5403 ms (39.07%), Space: 18.5 MB (95.59%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        l = len(words[0])
+        total = len(words) * l
+
+        def frequency_map(word_list):
+            freq = {}
+            for ch in word_list:
+                freq[ch] = freq.get(ch, 0) + 1
+            return freq
+        
+        main = frequency_map(words)
+        result = []
+        for start in range(0, len(s)- total +1):
+            s_parts = [s[i:i + l] for i in range(start, start + total, l)]
+            if frequency_map(s_parts) == main:
+                result.append(start)
+
+        return result
+        
