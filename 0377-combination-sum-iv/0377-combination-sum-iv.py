c869fc68e53096f12f7f738246228681902ec610
Lovy Verma <106698656+xtremislv@users.noreply.github.com>
2025-08-14 21:51:12 +0530

    Time: 44 ms (50.54%), Space: 17.7 MB (86.11%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def combinationSum4(self, nums, target):
+        dp = [1]
+        [
+            dp.append(sum(dp[i - x] for x in nums if i - x >= 0))
+            for i in range(1, target + 1)
+        ]
+        return dp[-1]
