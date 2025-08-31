282dd9df8b16191b60cf71fbf93655192e5e2e3c
Lovy Verma <106698656+xtremislv@users.noreply.github.com>
2025-08-31 16:04:55 +0530

    Time: 1034 ms (27.99%), Space: 21 MB (27.22%) - LeetHub

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+	    def common(chars1, chars2):
+		    for c1, c2 in zip(chars1, chars2):
+			    if c1 and c2: return True
+		    return False
+	    chars, ans = [[False]*26 for i in range(len(words))], 0
+	    for i, word in enumerate(words):
+		    for ch in word:
+			    chars[i][ord(ch) - ord('a')] = True
+		    for j in range(i):
+			    if not common(chars[i], chars[j]):
+				    ans = max(ans, len(words[i]) * len(words[j]))
+	    return ans
+        
