955807c98e736a26686752894f0408916dfb13ac
Lovy Verma <106698656+xtremislv@users.noreply.github.com>
2025-09-10 14:17:40 +0530

    Time: 0 ms (100%), Space: 17.7 MB (94.8%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.py
@@ -0,0 +1,10 @@
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        if turnedOn < 0 or turnedOn > 10 :
+            return []
+        result = []
+        for h in range(12):
+            for m in range(60):
+                if bin(h).count('1') + bin(m).count('1') == turnedOn:
+                    result.append("{}:{:02d}".format(h, m))
+        return result
