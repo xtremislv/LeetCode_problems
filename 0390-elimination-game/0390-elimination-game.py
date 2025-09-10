0cd8940592d831402c89c5897480bb5e41823a08
Lovy Verma <106698656+xtremislv@users.noreply.github.com>
2025-09-10 13:06:27 +0530

    Time: 0 ms (100%), Space: 17.9 MB (28.71%) - LeetHub

---
+++ 0390-elimination-game/0390-elimination-game.py
@@ -0,0 +1,8 @@
+class Solution:
+    def lastRemaining(self, n: int) -> int:
+        def helper(x, step, count, direction):
+            if count < 2:
+                return x
+            increment = step if direction or count % 2 else 0
+            return helper(x + increment, step * 2, count // 2, not direction)
+        return helper(1, 1, n, True)
