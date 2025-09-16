6f7d017d80758792c621571abd9794d8cb4b465d
Lovy Verma <106698656+xtremislv@users.noreply.github.com>
2025-09-16 13:36:19 +0530

    Time: 72 ms (61.76%), Space: 20.4 MB (50.18%) - LeetHub

---
+++ 0687-longest-univalue-path/0687-longest-univalue-path.py
@@ -0,0 +1,26 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def longestUnivaluePath(self, root):
+        self.longest_path = 0
+
+        def dfs(node):
+            if not node:
+                return 0
+
+            left_length = dfs(node.left)
+            right_length = dfs(node.right)
+
+            left_path = left_length + 1 if node.left and node.left.val == node.val else 0
+            right_path = right_length + 1 if node.right and node.right.val == node.val else 0
+
+            self.longest_path = max(self.longest_path, left_path + right_path)
+
+            return max(left_path, right_path)
+
+        dfs(root)
+        return self.longest_path
