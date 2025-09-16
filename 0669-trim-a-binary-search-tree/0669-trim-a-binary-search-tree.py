0b4dedc089997e1e78d364f469e4c231aaa7078c
Lovy Verma <106698656+xtremislv@users.noreply.github.com>
2025-09-16 13:26:54 +0530

    Time: 0 ms (100%), Space: 21.1 MB (29.58%) - LeetHub

---
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.py
@@ -0,0 +1,13 @@
+class Solution:
+    def trimBST(self, root, low, high):
+        if root is None:
+            return root
+        elif root.val < low:
+            return self.trimBST(root.right, low, high)
+        elif root.val > high:
+            return self.trimBST(root.left, low, high)
+        else:
+            root.left = self.trimBST(root.left, low, high)
+            root.right = self.trimBST(root.right, low, high)
+            return root
+            
